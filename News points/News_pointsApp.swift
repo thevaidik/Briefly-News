4df7b1eb1dc610972cad8be19598c1607a53275d
Vaidik <vaidik50000@gmail.com>
2025-01-13 22:30:47 +0530

    Initial Commit

---
+++ News points/News_pointsApp.swift
@@ -0,0 +1,17 @@
+//
+//  News_pointsApp.swift
+//  News points
+//
+//  Created by Vaidik Dubey on 13/01/25.
+//
+
+import SwiftUI
+
+@main
+struct News_pointsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
